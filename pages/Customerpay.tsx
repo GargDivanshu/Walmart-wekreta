28450d45c66871a8ce9fe205738d0cbc2c078018
ultigendlemate <chaitanya.anand@gmail.com>
2023-08-16 21:37:25 +0530

    miinor edits

--- pages/Customerpay.tsx
+++ pages/Customerpay.tsx
@@ -2,11 +2,11 @@ import React, {useEffect} from 'react'
 import Image from 'next/image'
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-import CheckoutForm from "../components/CheckoutForm";
+// import CheckoutForm from "../components/CheckoutForm";
 import {useRouter } from 'next/router'
 
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+// const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
 
 const Customerpay = () => {
