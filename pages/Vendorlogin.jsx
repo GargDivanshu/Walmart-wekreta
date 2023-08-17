c39f55c1ada68a5fd789cfa4c6840f33de33a2ed
Chaitanya Anand <92747604+UltigendLemate@users.noreply.github.com>
2023-08-18 00:11:22 +0530

    Domain removed

--- pages/Vendorlogin.jsx
+++ pages/Vendorlogin.jsx
@@ -26,7 +26,7 @@ export default function Vendorlogin() {
     });
 
     try {
-      const response = await fetch(`${process.env.DOMAIN}/api/login`, {
+      const response = await fetch(`/api/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
